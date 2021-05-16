0897e3dc5a50263e8880dace3ebdd0476cdacc75
yatirastogi <63449149+yatirastogi@users.noreply.github.com>
2021-05-17 00:01:58 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+  
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Search from './search';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './weather.css';
+
+ReactDOM.render(<Search/>, document.getElementById('root'));
